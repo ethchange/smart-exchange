6cfb827a7cf672b73086c9e02d5fddb952e9878d
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-05-24 01:40:37 +0200

    common setup, loading config file, jsonrpc requests relay, common jsonrpc server

---
+++ app.js
@@ -0,0 +1,54 @@
+var program = require('commander');
+var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
+var log = require('./lib/log');
+var server = require('./lib/server');
+var configPath = './config.json';
+
+/**
+ * Define program options
+ */
+program
+    .version('0.0.1')
+    .option('-c, --config <path>', 'set config path. default to ./config.json')
+    .parse(process.argv);
+
+configPath = program.config || configPath;
+
+/**
+ * Helper functions
+ */
+var toString = function (obj) {
+    return obj.toString();
+};
+
+log.info('1/2 Loading config file ' + configPath);
+
+fs.readFileAsync(configPath)
+.catch(function (err) {
+    log.error('Cannot load config file.');
+    process.exit(1);
+})
+.then(toString)
+.then(JSON.parse)
+.catch(function (err) {
+    log.error('Config file is not valid JSON file.');
+    process.exit(1);
+})
+.then(function (config) {  
+    var missingKeys = ['contract', 'jsonrpc', 'interface', 'port', 'ownerAddress'].filter(function (key) {
+        return config[key] === undefined;
+    });
+
+    if (missingKeys.length) {
+        log.error('Following required keys are not defined in config: ' + missingKeys.join(', '));
+        process.exit(1);
+    }
+
+    return config;
+}).then(function (config) {
+    log.info('2/2 Setting up ' + config.interface + ' interface on port ' + config.port);
+    server.listen(config);
+    log.info('Server Ready!');
+});
+
