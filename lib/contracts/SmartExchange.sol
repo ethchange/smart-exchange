685bd1eafa17b2058c12c8b083e01f80c0b60c8a
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-05-26 14:04:03 +0200

    ext_create

--- lib/contracts/SmartExchange.sol
+++ lib/contracts/SmartExchange.sol
@@ -2,27 +2,27 @@
 import "owned";
 
 contract SmartExchange is owned {
-	event OnAnonymousDeposit(address indexed from, uint value);
-	event OnDeposit(address indexed from, bytes32 indexed to, uint value);
-	event OnTransfer(bytes32 indexed from, address indexed to, uint value);
-	event OnIcapTransfer(bytes32 indexed from, address indexed to, bytes32 indirectId, uint value);
+	event AnonymousDeposit(address indexed from, uint value);
+	event Deposit(address indexed from, bytes32 indexed to, uint value);
+	event Transfer(bytes32 indexed from, address indexed to, uint value);
+	event IcapTransfer(bytes32 indexed from, address indexed to, bytes32 indirectId, uint value);
 
 	function () {
-		OnAnonymousDeposit(msg.sender, msg.value);
+		AnonymousDeposit(msg.sender, msg.value);
 	}
 
 	function deposit(bytes32 to) {
-		OnDeposit(msg.sender, to, msg.value);
+		Deposit(msg.sender, to, msg.value);
 	}
 
 	function transfer(bytes32 from, address to, uint value) onlyowner {
 		to.send(value);
-		OnTransfer(from, to, value);
+		Transfer(from, to, value);
 	}
 
 	function icapTransfer(bytes32 from, address to, bytes32 indirectId, uint value) onlyowner {
 		SmartExchange(to).deposit.value(value)(indirectId); // value?
-		OnIcapTransfer(from, to, indirectId, value);
+		IcapTransfer(from, to, indirectId, value);
 	}
 }
 
