ea0b6548bb05849a99b1caf77a284e5d38dfb2f7
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-07-17 12:31:04 +0200

    removed bluebird dependency

--- app.js
+++ app.js
@@ -1,62 +1,42 @@
 var program = require('commander');
-var Promise = require('bluebird');
-var fs = Promise.promisifyAll(require('fs'));
 var log = require('./lib/log');
 var server = require('./lib/server');
 var configPath = './config.json';
 
-/**
- * Define program options
- */
+// Define program options
 program
-    .version('0.0.1')
+    .version('0.1.0')
     .option('-c, --config <path>', 'set config path. default to ./config.json')
     .parse(process.argv);
 
 configPath = program.config || configPath;
 
-/**
- * Helper functions
- */
-var toString = function (obj) {
-    return obj.toString();
-};
-
+// Load config
 log.info('1/2 Loading config file ' + configPath);
+var config = require(configPath);
 
-fs.readFileAsync(configPath)
-.catch(function (err) {
-    log.error('Cannot load config file: ' + err.message);
+// Validate config
+var missingKeys = [
+    'contract', 'contract_name', 
+    'jsonrpc_host', 'jsonrpc_port',
+    'exchange_interface', 'exchange_port',
+    'owner'].filter(function (key) {
+    return config[key] === undefined;
+});
+
+if (missingKeys.length) {
+    log.error('Following required keys are not defined in config: ' + missingKeys.join(', '));
     process.exit(1);
-})
-.then(toString)
-.then(JSON.parse)
-.catch(function (err) {
-    log.error('Config file is not valid JSON file: ' + err.message);
-    process.exit(1);
-})
-.then(function (config) {  
-    var missingKeys = [
-        'contract', 'contract_name', 
-        'jsonrpc_host', 'jsonrpc_port',
-        'exchange_interface', 'exchange_port',
-        'owner'].filter(function (key) {
-        return config[key] === undefined;
-    });
+}
 
-    if (missingKeys.length) {
-        log.error('Following required keys are not defined in config: ' + missingKeys.join(', '));
-        process.exit(1);
-    }
-
-    return config;
-}).then(function (config) {
+// Start server
+try {
     log.info('2/2 Setting up ' + config.exchange_interface + ' interface on port ' + config.exchange_port);
     server.listen(config);
-}).catch(function (err) {
+} catch(err) {
     log.error('Error while settup up jsonrpc server: ' + err.message);
     process.exit(1);
-}).then(function () {
-    log.info('Server Ready!');
-});
+};
+
+log.info('Server Ready!');
 
