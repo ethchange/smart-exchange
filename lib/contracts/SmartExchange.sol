0d06d0f1c995ff9f108620e57f9af5d7e8e15331
IaroslavMazur <iaroslav.mazur@proton.me>
2023-05-14 19:47:49 +0300

    Update SmartExchange.sol

--- lib/contracts/SmartExchange.sol
+++ lib/contracts/SmartExchange.sol
@@ -2,27 +2,27 @@
 import "owned";
 
 contract SmartExchange is owned {
-	event AnonymousDeposit(address indexed from, uint value);
+	event AnonymousDeposit(address from, uint value) anonymous;
 	event Deposit(address indexed from, bytes32 indexed to, uint value);
 	event Transfer(bytes32 indexed from, address indexed to, uint value);
 	event IcapTransfer(bytes32 indexed from, address indexed to, bytes32 indirectId, uint value);
 
-	function () {
-		AnonymousDeposit(msg.sender, msg.value);
+	function anonymousDeposit() {
+		emit AnonymousDeposit(msg.sender, msg.value);
 	}
 
 	function deposit(bytes32 to) {
-		Deposit(msg.sender, to, msg.value);
+		emit Deposit(msg.sender, to, msg.value);
 	}
 
 	function transfer(bytes32 from, address to, uint value) onlyowner {
 		to.send(value);
-		Transfer(from, to, value);
+		emit Transfer(from, to, value);
 	}
 
 	function icapTransfer(bytes32 from, address to, bytes32 indirectId, uint value) onlyowner {
 		SmartExchange(to).deposit.value(value)(indirectId); // value?
-		IcapTransfer(from, to, indirectId, value);
+		emit IcapTransfer(from, to, indirectId, value);
 	}
 }
 
